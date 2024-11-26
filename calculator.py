9659b11cba522a0767a98bce04382baf1ad86bea
Ho Yi Victoria <s10243726@connect.np.edu.sg>
2024-11-26 14:10:53 +0800

    first commit

---
+++ calculator.py
@@ -0,0 +1,22 @@
+import math
+def addition(x,y):
+    return x+y
+
+def subtraction(x,y):
+    return x-y
+
+def multiplication(x,y):
+    return x*y
+
+def division(x,y):
+    return x/y
+
+""" def sin(x):
+    return math.sin(x)
+
+def cosine(x):
+    return math.cos(x)
+
+def tangent(x):
+    return math.tan(x) """
+
